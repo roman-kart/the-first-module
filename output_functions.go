520a3a3a49a796a99c25de54eeaaae401b7a43f3
roman-kart <rdkartashev@yandex.ru>
2023-09-18 22:03:41 +0300

    feat: add functions for output with separator

--- output_functions.go
+++ output_functions.go
@@ -1,8 +1,26 @@
 package the_first_module
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+var TxtSeparatorChar string = "-"
 
 func OutputToConsole(txt string) error {
 	_, err := fmt.Println(txt)
 	return err
+}
+
+func OutputSeparator() error {
+	err := OutputToConsole(strings.Repeat(TxtSeparatorChar, 30))
+	return err
+}
+
+func OutputToConsoleWithSeparator(txt string) error {
+	err := OutputToConsole(txt)
+	if err != nil {
+		return err
+	}
+	return OutputSeparator()
 }
